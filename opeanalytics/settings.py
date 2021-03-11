a4b1b2229875cd74924be25ffd6378b282041c43
rmessiass-FIT <renato.messias@aluno.faculdadeimpacta.com.br>
2021-03-11 10:06:37 -0300

    Ajustes Settings

--- opeanalytics/settings.py
+++ opeanalytics/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 
 import os
 
-import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
