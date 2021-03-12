656d6e1f66b1f3d32cb8cdc4082c578711f90da2
rmessiass-FIT <renato.messias@aluno.faculdadeimpacta.com.br>
2021-03-11 22:56:20 -0300

    Registro dos Modelos no core.admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Produto, OrderProduto, Pedido, Cliente, Pagamento
+from .models import Categoria, Produto, Cliente, Pagamento, Pedido, OrderProduto
 
 
 class ProdutoAdmin(admin.ModelAdmin):
@@ -11,8 +11,9 @@ class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone', 'creditlimit')
 
 
+admin.site.register(Categoria)
 admin.site.register(Produto, ProdutoAdmin)
-admin.site.register(OrderProduto)
-admin.site.register(Pedido)
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Pagamento)
+admin.site.register(Pedido)
+admin.site.register(OrderProduto)
